3153f75dbf3a209fc6c9faa5385d280edc09264c
user-faiz <email2faizm@gmail.com>
2023-12-01 22:44:45 +0530

    fixed linters error

--- my_list.rb
+++ my_list.rb
@@ -11,27 +11,3 @@ class MyList
     @list.each(&enum_methods)
   end
 end
-
-# Check the solutions
-
-# Create our list
-list = MyList.new(1, 2, 3, 4)
-
-# Test #all?
-puts(list.all? { |e| e < 5 })
-# true
-puts(list.all? { |e| e > 5 })
-# false
-
-# Test #any?
-puts(list.any? { |e| e == 2 })
-# true
-puts(list.any? { |e| e == 5 })
-# false
-
-# Test #filter
-print list.filter(&:even?)
-# [2, 4]
-# puts list.filter(&:even?)
-# 2
-# 4
